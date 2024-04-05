fee7eacc019f359a4461073f6a71953aa35c5c66
Denys Izhetskyi <denys.izhetskyi@booking.com>
2024-04-05 21:05:28 +0200

    fun

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,13 @@
-import type { MetaFunction } from "@vercel/remix";
+import type {
+  LinksFunction,
+  LoaderFunction,
+  MetaFunction,
+} from "@vercel/remix";
+import { sql } from "@vercel/postgres";
+import { useLoaderData } from "@remix-run/react";
+
+import styles from "../../public/style.css?url";
+import { useRef, useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +16,100 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+type UserData = { name: string; id: number; count: number };
+
+export const loader: LoaderFunction = async (args) => {
+  const { rows } = await sql`SELECT id, name, count FROM likes order by id;`;
+  return rows as UserData[];
+};
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+};
+
+function LikeButton({id}: {id: number}) {
+  const [loading, setLoading] = useState(false);
+
+  return <button disabled={loading} type="button" onClick={async (e) => {
+    e.stopPropagation();
+
+    const url = new URL('https://ne-viru.vercel.app/api/like');
+    url.searchParams.append('id', id.toString())
+
+    setLoading(true);
+    const r = await fetch(url, {headers: {"X-No-Redirect": "true"}});
+
+    if (r.ok) {
+      location.reload();
+    }
+  }}>
+    Like
+  </button>
+}
+
+function Users({ users }: { users: UserData[] }) {
+  return (
+    <table>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td width={50}>
+              <code>{user.count}</code>
+            </td>
+            <td>
+              <LikeButton id={user.id} />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+function NewUserForm() {
+  const [loading, setLoading] = useState(false);
+
+  const nameRef = useRef<HTMLInputElement>(null);
+  return (
+    <div>
+      <h2>Add new user</h2>
+      <input ref={nameRef} type="text" name="name" />
+      <button disabled={loading} type="button" onClick={async (e) => {
+        e.stopPropagation();
+        if (!nameRef.current) return;
+
+        const url = new URL('https://ne-viru.vercel.app/api/add');
+        url.searchParams.append('name', nameRef.current?.value)
+
+        setLoading(true);
+
+        const r = await fetch(url, {headers: {"x-no-redirect": "true"}});
+
+        if (r.ok) {
+          location.reload();
+        }
+      }}>Add</button>
+    </div>
+  );
+}
+
 export default function Index() {
+  const data: UserData[] = useLoaderData<typeof loader>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>Little likes app</h1>
+      <form action="https://ne-viru.vercel.app/api/like">
+        {data.length ? <Users users={data} /> : "No users yet :)"}
+      </form>
+      <br />
+      <NewUserForm />
     </div>
   );
 }
